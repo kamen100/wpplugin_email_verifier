edc752d800f65c66aa770ad6d96e1b615a909bc4
Камен Петков <petkov6@gmail.com>
2018-11-06 15:53:46 +0200

    Delete content
    Added admin menu and delete content

--- options-page-wraper.php
+++ options-page-wraper.php
@@ -1,10 +1,7 @@
-
-<h2><?php  esc_attr_e( '2 Columns Layout: static (px)', 'WpAdminStyle' ); ?></h2>
-
 <div class="wrap">
 
 	<div id="icon-options-general" class="icon32"></div>
-	<h1><?php esc_attr_e( 'Heading', 'WpAdminStyle' ); ?></h1>
+	<!-- <h1><?php// esc_attr_e( 'Heading', 'WpAdminStyle' ); ?></h1> -->
 
 	<div id="poststuff">
 
@@ -23,11 +20,11 @@
 						</button>
 						<!-- Toggle -->
 
-						<h2 class="hndle"><span><?php esc_attr_e( 'Main Content Header', 'WpAdminStyle' ); ?></span>
+						<h2 class="hndle"><span><?php esc_attr_e( 'Email Verifier', 'WpAdminStyle' ); ?></span>
 						</h2>
 
 						<div class="inside">
-							<p><?php esc_attr_e( 'WordPress started in 2003 with a single bit of code to enhance the typography of everyday writing and with fewer users than you can count on your fingers and toes. Since then it has grown to be the largest self-hosted blogging tool in the world, used on millions of sites and seen by tens of millions of people every day.',
+							<p><?php esc_attr_e( 'Email spam control.',
 							                     'WpAdminStyle' ); ?></p>
 						</div>
 						<!-- .inside -->
@@ -59,7 +56,7 @@
 								); ?></span></h2>
 
 						<div class="inside">
-							<p><?php esc_attr_e( 'Everything you see here, from the documentation to the code itself, was created by and for the community. WordPress is an Open Source project, which means there are hundreds of people all over the world working on it. (More than most commercial platforms.) It also means you are free to use it for anything from your cat’s home page to a Fortune 500 web site without paying anyone a license fee and a number of other important freedoms.',
+							<p><?php esc_attr_e( 'Everything you see here, are freedoms.',
 							                     'WpAdminStyle' ); ?></p>
 						</div>
 						<!-- .inside -->
